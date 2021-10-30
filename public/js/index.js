a718e41845929aca063b410aab2058f90cdafe6b
Pranav <pranvofficial@gmail.com>
2021-10-30 22:53:21 +0530

    minor bug fixes

--- public/js/index.js
+++ public/js/index.js
@@ -101,3 +101,11 @@ document.getElementById('leave-btn').addEventListener('click', () => {
     }
 });
 
+document.getElementById('res-leave-btn').addEventListener('click', () => {
+    const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+    if (leaveRoom) {
+        window.location = '../index.html';
+    } else {
+    }
+});
+
